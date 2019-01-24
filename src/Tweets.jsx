d01f38b986ec85e3a290923500df653696e6ffd6
Haggis Breakfast <lj.stewart00@gmail.com>
2019-01-24 12:55:03 -0800

    adds comments

--- src/Tweets.jsx
+++ src/Tweets.jsx
@@ -14,10 +14,12 @@ export default class Tweets extends Component {
   render() {
     return (
       <TweetContainer>
+        {/* map over tweets array to spit out individual tweets */}
         {tweets.map((tweet, i) => {
           return (
             <Tweet>
               {' '}
+              {/* give each tweet stringified JSON data and individual key */}
               tweet={JSON.stringify(tweet)} key={i}{' '}
             </Tweet>
           );
